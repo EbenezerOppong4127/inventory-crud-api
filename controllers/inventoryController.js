2e06a5be4a4d76e85d47ff9b75b2569ffe986269
EbenezerOppong4127 <tro.jule@gmail.com>
2025-03-29 05:08:21 +0000

    pushw4.2

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,4 +1,4 @@
-const Inventory = require('../models/inventory');
+const Inventory = require('../models/Inventory');
 const { inventorySchema } = require('../validation/inventoryValidation');
 const { AppError } = require('../middlewares/errorMiddleware');
 
